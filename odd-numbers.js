3408ccf0fd362ce724da3e9573e77f12d724a137
Siam-moni <shariars012@gmail.com>
2026-01-31 20:37:58 +0600

    odd-even-numbers

---
+++ odd-numbers.js
@@ -0,0 +1,15 @@
+let i = 61;
+while (i <= 100) {
+  if (i % 2 === 1) {
+    console.log("odd numbers:", i);
+  }
+  i++;
+}
+
+let n = 78;
+while (n <= 98) {
+  if (n % 2 === 0) {
+    console.log("even numbers:", n);
+  }
+  n++;
+}
